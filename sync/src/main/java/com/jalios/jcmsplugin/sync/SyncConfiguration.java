2b6d6a93a3f314d7b38df52a51fedf880f75d5ab
lxtuong <lxtuong@gmail.com>
2014-03-10 23:58:51 +0100

    use configurator

--- sync/src/main/java/com/jalios/jcmsplugin/sync/SyncConfiguration.java
+++ sync/src/main/java/com/jalios/jcmsplugin/sync/SyncConfiguration.java
@@ -1,5 +1,52 @@
 package com.jalios.jcmsplugin.sync;
 
-public class SyncConfiguration {
+import java.io.File;
 
+public final class SyncConfiguration {
+  private File ppRootDir;
+  private File wpRootDir;
+  private String confPath;
+  private boolean preview = false;
+  
+  private SyncConfiguration(Builder b) {
+    this.ppRootDir = b.ppRootDir;
+    this.wpRootDir = b.wpRootDir;
+    this.confPath = b.confPath;
+  }
+
+  public File getPluginProjectRootDir() {
+    return ppRootDir;
+  }
+
+  public File getWebappProjectRootDir() {
+    return wpRootDir;
+  }
+  
+  public void enablePreview(){
+    this.preview = true;
+  }
+ 
+  public boolean isPreviewEnabled(){
+    return this.preview;
+  }
+
+  public static class Builder {
+    private File ppRootDir;
+    private File wpRootDir;
+    private String confPath;
+
+    public Builder(File ppRootDir, File wpRootDir) {
+      this.ppRootDir = ppRootDir;
+      this.wpRootDir = wpRootDir;
+    }
+
+    public Builder conf(String confPath) {
+      this.confPath = confPath;
+      return this;
+    }
+
+    public SyncConfiguration build() {
+      return new SyncConfiguration(this);
+    }
+  }
 }
